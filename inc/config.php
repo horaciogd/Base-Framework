df074ce84574ba3dee3b6f127c9805982151e4db
horaciogd <horaciogd@vhplab.net>
2015-01-02 19:12:15 +0100

    Initial Commit - classic config file
    Base php-mysql-json framework for avoiding CMS in small projects

---
+++ inc/config.php
@@ -0,0 +1,19 @@
+<?php
+
+/* DATABASE INFORMATION */
+$db = array(
+	"type" => "mysql",
+	"host" => "**********",
+	"user" => "**********",
+	"pass" => "**********"
+);
+
+$db_tables = array(
+	"name" => "base_name",
+);
+
+/* Global variables */
+$root = "http://www.**********/";
+
+
+?>
